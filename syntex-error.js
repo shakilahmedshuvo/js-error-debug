e64d1eb7eaebc05ce63159ef4e58f41126fcb403
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-03-08 23:40:10 +0600

    commit

--- syntex-error.js
+++ syntex-error.js
@@ -1,1 +1,27 @@
-console.log('hi')
+// error:1
+function isBigger(num) {
+    if (num > 5) {
+        return true;
+    }
+    else return false;
+}
+// }
+const result = isBigger(11);
+// console.log(result);
+
+// error:2
+// const sum = a, b => a + b;
+const sum = (a, b) => a + b;
+const res = sum(3, 5);
+
+const musk = {
+    name: 'elon',
+    // age: 53
+    age: 53,
+    job: 'tesla'
+}
+// console.log(musk.age);
+for (let i = 0; i < 10; i++) {
+    // const n = numbers [i;
+    const n = numbers[i];
+}
