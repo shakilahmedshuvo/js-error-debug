bdca2d75ac45bd15f14a4c0a28626f689345b588
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-03-08 23:57:00 +0600

    type error

---
+++ type-error.js
@@ -0,0 +1,12 @@
+const numbs = 5;
+// numbs.push(45);
+
+
+// let adderss;
+// console.log(adderss.home);
+let adderss = { city: 'bBaria', postCode: 4560 };
+// console.log(adderss.home.bulding);
+console.log(adderss?.home?.bulding);
+
+const a = 'abul';
+// a = 'babul';
