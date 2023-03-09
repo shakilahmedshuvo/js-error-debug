c00fba17396a5199cb8a59de94d4bf70b1456cc9
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-03-09 12:28:30 +0600

    3

--- referance-error.js
+++ referance-error.js
@@ -3,4 +3,18 @@ const y = 'moni';
 // console.log(z);
 
 // console.log(name);
-const name = 'Ananta Jalil';
+// const name = 'Ananta Jalil';
+// let name = 'Ananta Jalil';
+// var name = 'Ananta Jalil';
+
+// call the function
+// console.log(add(2, 3))
+
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+// call the function add2
+console.log(add2(2, 3))
+const add2 = (n1, n2) => n1 + n2;
+
